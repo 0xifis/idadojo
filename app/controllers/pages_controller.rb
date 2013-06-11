a19306acc39dac309c25f31c1b09755e563eeab0
Vishnu R Menon <vishthemenon@gmail.com>
2013-06-11 14:42:51 +0800

    some stuff

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   before_filter :authenticate_user!, except: [:home, :contact]
   def home
-    @posts = Post.order("created_at DESC").all
+    @posts = Post.order("created_at ASC").all
   end
 
   def contact
